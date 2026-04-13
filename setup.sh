d9dcbf3142a44fe6ce1df6dd5f75b28cf626f2ff
GuoBinZhuang <2533164055@qq.com>
2026-04-13 23:02:40 +0800

    个人环境适配修改

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "Setup unitree ros2 environment"
-source /opt/ros/foxy/setup.bash
-source $HOME/unitree_ros2/cyclonedds_ws/install/setup.bash
+source /opt/ros/humble/setup.bash
+
+# Optional: source local cyclonedds overlay only when it exists.
+if [ -f "$HOME/unitree_ros2/cyclonedds_ws/install/setup.bash" ]; then
+    source "$HOME/unitree_ros2/cyclonedds_ws/install/setup.bash"
+fi
+
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-                            <NetworkInterface name="enp3s0" priority="default" multicast="default" />
+                            <NetworkInterface name="eth2" priority="default" multicast="default" />
                         </Interfaces></General></Domain></CycloneDDS>'
